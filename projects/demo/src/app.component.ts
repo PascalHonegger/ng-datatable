b358818734190618791cac5f076d3fa2aeb3c75a
tomaz <tomaz.majerhold@arnes.si>
2025-06-21 13:02:32 +0200

    🛠️ demo import

--- projects/demo/src/app.component.ts
+++ projects/demo/src/app.component.ts
@@ -1,52 +1,57 @@
 import { Component, OnInit, inject } from "@angular/core";
-import {HttpClient} from "@angular/common/http";
-import {DataTableModule, SortBy, SortOrder} from "ng-datatable";
+import { HttpClient } from "@angular/common/http";
+import {
+  BootstrapPaginator,
+  DataTable,
+  DefaultSorter,
+  SortBy,
+  SortOrder,
+} from "ng-datatable";
 import { FormsModule } from "@angular/forms";
 import { DataFilterPipe } from "./data-filter.pipe";
 import { UpperCasePipe } from "@angular/common";
 
 @Component({
-    selector: "app-root",
-    templateUrl: "./app.component.html",
-    imports: [
-        DataTableModule,
-        FormsModule,
-        DataFilterPipe,
-        UpperCasePipe
-    ]
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  imports: [
+    FormsModule,
+    DataFilterPipe,
+    UpperCasePipe,
+    DataTable,
+    DefaultSorter,
+    BootstrapPaginator,
+  ],
 })
 export class AppComponent implements OnInit {
-    private http = inject(HttpClient);
+  private http = inject(HttpClient);
 
+  public data: any[];
+  public filterQuery = "";
+  public rowsOnPage = 10;
+  public sortBy: SortBy = "email";
+  public sortOrder: SortOrder = "asc";
 
-    public data: any[];
-    public filterQuery = "";
-    public rowsOnPage = 10;
-    public sortBy: SortBy = "email";
-    public sortOrder: SortOrder = "asc";
+  ngOnInit(): void {
+    this.http.get<any[]>("/data.json").subscribe((data) => {
+      setTimeout(() => {
+        this.data = data;
+      }, 2000);
+    });
+  }
 
-    ngOnInit(): void {
-        this.http.get<any[]>("/data.json")
-            .subscribe((data) => {
-                setTimeout(() => {
-                    this.data = data;
-                }, 2000);
-            });
+  public toInt(num: string) {
+    return +num;
+  }
+
+  public sortByWordLength = (a: any) => {
+    return a.city.length;
+  };
+
+  public remove(item: any) {
+    const index = this.data.indexOf(item);
+    if (index > -1) {
+      this.data.splice(index, 1);
     }
-
-    public toInt(num: string) {
-        return +num;
-    }
-
-    public sortByWordLength = (a: any) => {
-        return a.city.length;
-    }
-
-    public remove(item: any) {
-        const index = this.data.indexOf(item);
-        if (index > -1) {
-            this.data.splice(index, 1);
-        }
-    }
-
+  }
 }
